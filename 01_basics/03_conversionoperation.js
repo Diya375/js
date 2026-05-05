d09fdcde301998eb916cd2d19b08b986ece42045
Diya375 <diyasubedi5@gmail.com>
2026-05-05 07:01:23 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score = "diya"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//Nan=not a number
+
+//"33"=> 33
+//"33abc"=> NaN
+//true=>1; false=>0
+let isLoggedin =  ""
+let booleanisLoggedin= Boolean(isLoggedin)
+console.log(booleanisLoggedin);
+
+//1->true
+//""->false
+//"diya"->true
+
+let someNumber=33
+
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
