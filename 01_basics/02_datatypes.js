7072708985c31695ce950a4c53bf03bee7652231
Diya375 <diyasubedi5@gmail.com>
2026-05-05 06:34:32 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+// "use strict"     //use all js code as newer version
+
+// alert( 3 + 3) //we're using nodejs not browser
+
+console.log(3 + 3)
+
+console.log("Diya")
+
+let name="Diya"
+let age=22
+let isloggedin=false
+let state;
+
+//number 2 to the power 53
+//bigint
+//string=""
+//boolean=true/false
+//null=standaalone value
+//undefined=>
+//symbol=>unique
+
+console.log(typeof age)
+console.log(typeof undefined) //undefined
+console.log(typeof null)  //object
