a2ec9ebb337ca1b90801f059b2f57155b004d7b8
Diya375 <diyasubedi5@gmail.com>
2026-05-05 07:30:20 +0000

    conversion of datatypes

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -24,3 +24,43 @@ let someNumber=33
 let stringNumber= String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//**********Operations***********//
+
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 3)
+// console.log(2 / 2)
+// console.log(2 % 2)
+
+let str1="hello"
+let str2="diya"
+let str3= str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);// gives 12
+// console.log(1 + "2");//gives 12
+// console.log(1 + 2 + "2"); //give 32
+ 
+// console.log((3+4)*5%3); //gives 2
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1= num2= num3 =2 + 2
+
+let gamecounter =100
+++gamecounter;
+console.log(gamecounter);
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html
+
+
