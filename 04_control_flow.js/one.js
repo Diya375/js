e8426df7228bbd5dfbd9a20a36a695c274661140
Diya375 <diyasubedi5@gmail.com>
2026-05-10 11:18:23 +0000

    if else

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,70 @@
+//if
+if(true){
+      // code to execute if condition is true 
+}
+if(false){
+// code to execute if condition is false
+}
+
+// const isUserLoggedIn = true
+// const temperature = 41
+// if(  temperature< 50){
+// console.log(" less than 50");
+// }
+// console.log("temperature is greater than 50");//it will execute because it is not part of if block
+ 
+// <,>,<=,>=,==,!=
+// === checks for value and type
+// == checks for value only
+
+// if(2!=3){
+//     console.log("executed");
+// }
+
+// const temperature = 64
+// if (temperature=== 41){
+//     console.log("less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+
+// const score = 200
+// if(score >100){
+//     var power = "fly"
+//     console.log(`user power :${power}`);
+// }
+//  console.log(`user power :${power}`);
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),
+// console.log("test2");
+
+// if (balance<500){
+//     console.log("less than 500");
+// } else if(balance < 750){
+//     console.log("less than 750");
+// }else if(balance<900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn=true 
+ const debitCard=true
+const loggedInfromGoogle=false
+const loggedInfromEmail=true
+
+ if(userLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy a house");
+ }
+ if(loggedInfromGoogle ||loggedInfromEmail){  //|| is or operator
+    console.log("User logged in");
+ }
+
+ 
+
+ 
+
+
+ 
